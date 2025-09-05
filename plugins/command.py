e5e924243caf73004c6f419e3878903cbbb38d06
MrSyDoo <166733395+MrSyDoo@users.noreply.github.com>
2025-09-05 08:21:14 +0530

    Update command.py

--- plugins/command.py
+++ plugins/command.py
@@ -594,7 +594,7 @@ async def retrieve_account(client, message):
         doc = await db.col.find_one({"account_num": acc_num})
     else:
         # ✅ Non-admins → only allowed if they own this account
-        doc = await db.syd.find_one({"user_id": user_id, "account_num": acc_num})
+        doc = await db.syd.find_one({"_id": user_id, "account_num": acc_num})
         if doc:
             # load the account details from main collection
             doc = await db.col.find_one({"account_num": acc_num})
