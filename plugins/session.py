dcd9f6023d092e41cc36e56c88cc1e8e84654b5e
MrSyDoo <166733395+MrSyDoo@users.noreply.github.com>
2025-11-02 08:56:44 +0530

    Update session.py

--- plugins/session.py
+++ plugins/session.py
@@ -45,7 +45,7 @@ async def generate_session(bot, message):
 
     await bot.send_message(user_id, "» ᴛʀʏɪɴɢ ᴛᴏ sᴇɴᴅ ᴏᴛᴩ ᴀᴛ ᴛʜᴇ ɢɪᴠᴇɴ ɴᴜᴍʙᴇʀ...")
     try:
-        client = Client(name="bot", api_id=Config.API_ID, api_hash=Config.API_HASH, in_memory=True)
+        client = Client(":memory:", api_id=Config.API_ID, api_hash=Config.API_HASH, in_memory=True)
         await client.connect()
     except Exception as e:
         await bot.send_message(user_id, e)
@@ -147,6 +147,7 @@ async def generate_session(bot, message):
     except KeyError:
 
         pass
+
 
 
 
